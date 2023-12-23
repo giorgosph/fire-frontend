1676c01e085dc48179be5f1404db52835bf94094
Giorgos Philippou <g.philippou@student.reading.ac.uk>
2023-12-23 20:40:06 +0200

    initial from structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [formFields, setFormFields] = useState([
+    { id: 1, title: '', height: '', width: '', distance: '', originalValues: true },
+  ]);
+
+  const handleInputChange = (index, event) => {
+    const { name, value } = event.target;
+    const updatedFields = [...formFields];
+    updatedFields[index][name] = value;
+    setFormFields(updatedFields);
+  };
+
+  const handleAddRow = () => {
+    const newId = formFields[formFields.length - 1].id + 1;
+    setFormFields([
+      ...formFields,
+      { id: newId, title: '', height: '', width: '', distance: '', originalValues: true },
+    ]);
+  };
+
+  const handleRemoveRow = (id) => {
+    const updatedFields = formFields.filter((field) => field.id !== id);
+    setFormFields(updatedFields);
+  };
+
+  const handleUpdateOriginal = (index) => {
+    const updatedFields = [...formFields];
+    updatedFields[index].originalValues = false;
+    setFormFields(updatedFields);
+  };
+
+  const handleSubmit = () => {
+    // Handle form submission logic here
+    console.log('Form submitted:', formFields);
+    // You can send the formFields data to your backend or perform any necessary actions
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {formFields.map((field, index) => (
+        <div key={field.id}>
+          <input
+            type="text"
+            placeholder="Title"
+            name="title"
+            value={field.title}
+            onChange={(e) => handleInputChange(index, e)}
+          />
+          <input
+            type="text"
+            placeholder="Height"
+            name="height"
+            value={field.height}
+            onChange={(e) => handleInputChange(index, e)}
+          />
+          <input
+            type="text"
+            placeholder="Width"
+            name="width"
+            value={field.width}
+            onChange={(e) => handleInputChange(index, e)}
+          />
+          <input
+            type="text"
+            placeholder="Distance"
+            name="distance"
+            value={field.distance}
+            onChange={(e) => handleInputChange(index, e)}
+          />
+          {index > 0 && (
+            <button onClick={() => handleRemoveRow(field.id)}>Remove</button>
+          )}
+          <button onClick={() => handleUpdateOriginal(index)}>Update Original</button>
+        </div>
+      ))}
+      <button onClick={handleAddRow}>+</button>
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
-}
+};
 
 export default App;
